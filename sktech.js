aca02f7a6825e81d37125c18f06f1e7d30aa99b2
nygds <148563203+nygds@users.noreply.github.com>
2023-10-27 12:12:44 -0300

    adicionando arquivo js e css

---
+++ sktech.js
@@ -0,0 +1,21 @@
+function setup(){
+  
+    createCanvas(600, 600);
+    
+    background('pink');
+    
+    }
+  
+  function draw() {
+    
+    stroke('blue');
+    
+    fill('purple');
+    
+    if (mouseIsPressed){
+      
+      rect(mouseX, mouseY,50, 65);
+      
+  }
+    
+  }
